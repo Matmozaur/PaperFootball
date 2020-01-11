3cfd75c227daaa068515ac9c530626789e56ba8a
idabrows <daiz@op.pl>
2020-01-11 19:46:46 +0100

    gui with menu

--- view/board_gui.py
+++ view/board_gui.py
@@ -1,12 +1,15 @@
 import doctest
 import pickle
+import webbrowser
 from _tkinter import TclError
-from tkinter import Button
+from tkinter import Button, BOTTOM, BOTH
 import tkinter as tk
 from model.ml_module.dummy_models import RandomModel, ForwardModel, BackwardModel
 from view.board import Board
 from model.ml_module.agent import Agent
 from model.game import Game
+
+LARGE_FONT = ("Verdana", 12)
 
 
 def turn_path(path):
@@ -23,18 +26,19 @@ class BoardGui:
         self.agent1 = None
         self.agent2 = None
         self.bot = None
-        # player1_file = open("temp_fw_trained", "rb")
-        # player2_file = open("temp_fw_trained", "rb")
-        # self.player1 = pickle.load(player1_file)
-        # self.player2 = pickle.load(player2_file)
+        self.player1 = None
+        self.player2 = None
 
     def start(self):
+        player1_file = open("temp_fw_trained", "rb")
+        player2_file = open("temp_fw_trained", "rb")
+        self.player1 = pickle.load(player1_file)
+        self.player2 = pickle.load(player2_file)
+
         self.root = tk.Tk()
         file = open('../controller/temp_fw_trained', 'rb')
         self.bot = pickle.load(file)
         # self.bot = Agent('dsdas', model=ForwardModel())
-
-        self.set_game_window()
 
         self.set_panel()
 
@@ -78,67 +82,11 @@ class BoardGui:
         if self.env.currentPlayer == -1:
             self.get_move()
 
-    def new_game(self):
-        try:
-            self.window.destroy()
-        except TclError:
-            print("Game window closed")
-        finally:
-            self.window = tk.Tk()
-            self.set_game_window()
-            self.agent = self.bot
-            self.env = Game()
-            self.board = Board(self.window)
-            self.board.draw_board()
-            self.board.color_current_point()
-            self.board.color_allowable_points()
-            self.board.canvas.pack(fill="both", expand=True)
-            # self.board.canvas.bind('<>', self.get_move())
-            self.board.canvas.bind('<Button>', self.on_click)
-
-    def set_game_window(self):
-        self.window.geometry("400x600")
-
-    def set_panel(self):
-        frame = tk.Frame(self.root)
-        frame.pack()
-        button_new_game = Button(frame, text="New game", command=self.new_game, width="25")
-        button_new_robot_fight = Button(frame, text="New robot fight", command=self.new_game_bot, width="25")
-        button_next_robot_turn = Button(frame, text="Next move", command=self.next_move_bot, width="25")
-        button_quit = Button(frame, text="Quit", command=self.close_windows, width="25")
-        button_new_game.pack()
-        button_new_robot_fight.pack()
-        button_next_robot_turn.pack()
-        button_quit.pack()
-        self.root.mainloop()
-
-    def new_game_bot(self):
-        try:
-            self.window.destroy()
-        except TclError:
-            print("Game window closed")
-        finally:
-            self.window = tk.Tk()
-            self.set_game_window()
-            self.agent1 = self.bot
-            self.agent2 = self.bot
-            self.env = Game()
-            self.board = Board(self.window)
-            self.board.draw_board()
-            self.board.color_current_point()
-            self.board.color_allowable_points()
-            self.board.canvas.pack(fill="both", expand=True)
-
     def next_move_bot(self):
         if self.env.currentPlayer == 1:
             self.get_move(self.agent1)
         else:
             self.get_move(self.agent2)
-
-
-    def close_windows(self):
-        self.window.destroy()
-        self.root.destroy()
 
     @staticmethod
     def get_positions(x, y):
@@ -199,12 +147,122 @@ class BoardGui:
         self.env.gameState.board[21, 4] = 1
         self.env.gameState.current_position = (4, 3)
         # print([self.env.gameState.get_move(x,y) for x,y in [[(4,3),(5,3)],[(5,3),(5,4)],[(5,4),(6,4)]]])
-        print(self.env.gameState.get_move((6,4),(5,4)))
+        print(self.env.gameState.get_move((6, 4), (5, 4)))
         # print(self.env.gameState.get_full_moves())
-        self.board.draw_lines([self.get_positions(x,y) for x,y in [(21, 4), (20, 3)]])
+        self.board.draw_lines([self.get_positions(x, y) for x, y in [(21, 4), (20, 3)]])
         # self.board.draw_lines([[(4,3),(5,3)],[(5,3),(5,4)],[(5,4),(6,4)]])
         self.window.mainloop()
 
-#
-# b = BoardGui()
-# b.test_board()
+
+class GameWindow(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        container = tk.Frame(self)
+        container.pack(fill=BOTH, expand=True)
+
+        self.geometry("400x700")
+        self.frames = {}
+
+        for F in (Menu, NewGameBU, BBMenu, NewGameBB):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+        self.show_frame(Menu)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+class Menu(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Paper Football", font=LARGE_FONT)
+        label.pack()
+
+        button_new_game = tk.Button(self, text="New Game user-bot",
+                                    command=lambda: controller.show_frame(NewGameBU), width="25")
+        button_new_game.pack()
+
+        button_new_bot_game = tk.Button(self, text="New Game bot-bot",
+                                        command=lambda: controller.show_frame(BBMenu), width="25")
+        button_new_bot_game.pack()
+
+        button_instructions = tk.Label(self, text="How To Play", fg="blue", cursor="hand2", width="25")
+        button_instructions.pack()
+        button_instructions.bind("<Button-1>", lambda e: webbrowser.open_new("https://en.wikipedia.org/wiki/Paper_soccer"))
+
+        button_instructions.pack()
+
+        button_quit = tk.Button(self, text="Quit",
+                                command=self.destroy, width="25")
+        button_quit.pack()
+
+
+class NewGameBU(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.boardGui = BoardGui()
+        self.boardGui.file = open('../controller/temp_fw_trained', 'rb')
+        self.boardGui.bot = pickle.load(self.boardGui.file)
+        self.boardGui.agent = self.boardGui.bot
+        self.boardGui.env = Game()
+        self.boardGui.board = Board(self)
+        self.boardGui.board.draw_board()
+        self.boardGui.board.color_current_point()
+        self.boardGui.board.color_allowable_points()
+        self.boardGui.board.canvas.config(width=400, height=600)
+        self.boardGui.board.canvas.bind('<Button>', self.boardGui.on_click)
+        self.boardGui.board.canvas.pack(fill = BOTH, expand = True)
+        button1 = tk.Button(self, text="Restart Game",
+                            command=lambda: controller.show_frame(NewGameBU))
+        button1.pack(side=BOTTOM)
+
+        button2 = tk.Button(self, text="Return",
+                            command=lambda: controller.show_frame(Menu))
+        button2.pack(side=BOTTOM)
+
+
+class NewGameBB(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Page Two!!!", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        button1 = tk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(Menu))
+        button1.pack()
+
+        button2 = tk.Button(self, text="Page One",
+                            command=lambda: controller.show_frame(NewGameBU))
+        button2.pack()
+
+
+class BBMenu(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label1 = tk.Label(self, text="Player 1", font=LARGE_FONT)
+        label1.pack(pady=10, padx=10)
+        variable1 = tk.StringVar(self)
+        variable1.set("Random")
+        p1 = tk.OptionMenu(self, variable1, "Random", "Forward", "Intelligent")
+        p1.pack()
+        label2 = tk.Label(self, text="Player 2", font=LARGE_FONT)
+        label2.pack(pady=10, padx=10)
+        variable2 = tk.StringVar(self)
+        variable2.set("Random")
+        p2 = tk.OptionMenu(self, variable2, "Random", "Forward", "Intelligent")
+        p2.pack()
+        button_play = tk.Button(self, text="Play",
+                            command=lambda: controller.show_frame(NewGameBB))
+        button_play.pack()
+
+
+app = GameWindow()
+app.mainloop()
